0ab56160261f623b94479ac6e6040c1604c5b63a
Karan Samel <samelkaran@gmail.com>
2014-11-01 12:13:54 -0400

    Update testSudoku.java
    added more methods, we have to test all methods and constructors

--- testSudoku.java
+++ testSudoku.java
@@ -1,3 +1,5 @@
+import static org.junit.Assert.*;
+import java.util.*;
 import junit.framework.TestCase;
 
 public class TestSudoku extends TestCase {
@@ -15,6 +17,57 @@ public class TestSudoku extends TestCase {
     s3 = new Sudoku(input2);
   }
   
+  public void testBoard() {
+    int[][] array = {
+      {7, 3, 5, 1, 6, 4, 9, 2, 8}, 
+      {4, 2, 6, 9, 7, 8, 3, 1, 5},
+      {1, 9, 8, 5, 3, 2, 6, 7, 4},
+      {2, 4, 9, 3, 8, 1, 7, 5, 6},
+      {3, 8, 7, 2, 5, 6, 1, 4, 9},
+      {5, 6, 1, 7, 4, 9, 8, 3, 2},
+      {8, 5, 2, 6, 1, 7, 4, 9, 3},
+      {9, 1, 4, 8, 2, 3, 5, 6, 7},
+      {6, 7, 3, 4, 9, 5, 2, 8, 1}
+    };
+    assertArrayEquals(array,array);
+  }
+  
+  public void testStringToBoard() {
+    int[][] array = {
+      {7, 3, 5, 1, 6, 4, 9, 2, 8}, 
+      {4, 2, 6, 9, 7, 8, 3, 1, 5},
+      {1, 9, 8, 5, 3, 2, 6, 7, 4},
+      {2, 4, 9, 3, 8, 1, 7, 5, 6},
+      {3, 8, 7, 2, 5, 6, 1, 4, 9},
+      {5, 6, 1, 7, 4, 9, 8, 3, 2},
+      {8, 5, 2, 6, 1, 7, 4, 9, 3},
+      {9, 1, 4, 8, 2, 3, 5, 6, 7},
+      {6, 7, 3, 4, 9, 5, 2, 8, 1}
+    };
+    String s = "735164928426978315198532674249381756387256149561749832852617493914823567673495281";
+    assertArrayEquals(array, s3.stringToBoard(s));
+  }
+  
+  public void testGetBox() {
+    int[][] array = {
+      {7, 3, 5, 1, 6, 4, 9, 2, 8}, 
+      {4, 2, 6, 9, 7, 8, 3, 1, 5},
+      {1, 9, 8, 5, 3, 2, 6, 7, 4},
+      {2, 4, 9, 3, 8, 1, 7, 5, 6},
+      {3, 8, 7, 2, 5, 6, 1, 4, 9},
+      {5, 6, 1, 7, 4, 9, 8, 3, 2},
+      {8, 5, 2, 6, 1, 7, 4, 9, 3},
+      {9, 1, 4, 8, 2, 3, 5, 6, 7},
+      {6, 7, 3, 4, 9, 5, 2, 8, 1}
+    };
+    int[][] box = {
+      {7, 3, 5},
+      {4, 2, 6},
+      {1, 9, 8}
+    };
+    Sudoku s4 = new Sudoku(array);
+    assertArrayEquals(box,s4.getBox(1,1));
+  }
   public void testCandidates() {
     boolean[] temp;
     temp = s.candidates(0, 0);
@@ -29,6 +82,15 @@ public class TestSudoku extends TestCase {
     assertEquals(temp[7], false);
     assertEquals(temp[8], true);
     assertEquals(temp[9], true);
+  }
+  
+  public void testGetNumsFromCandidates() {
+    ArrayList<Integer> l = new ArrayList<Integer>();
+    ArrayList<Integer> t = new ArrayList<Integer>();
+    t.add(6);
+    t.add(9);
+    s.getNumsFromCandidates(s.candidates(6,0),l);
+    assertEquals(t,l);
   }
   
   public void testHiddenSingles() {
@@ -46,7 +108,6 @@ public class TestSudoku extends TestCase {
   public void testIsSolved() {
     assertFalse(s.isSolved());
   }
-  
   public void testSolve() {
     int[][] actualSolution = {
       {7, 3, 5, 1, 6, 4, 9, 2, 8}, 
